f898b23c536420d840eed0cd87c8916d6bc066b6
nattaaek <w.nattaaek@yahoo.com>
2017-06-25 01:30:17 +0700

    add service worker offline

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+importScripts('/cache-polyfill.js');
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('random').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/index.html?homescreen=1',
+                '/?homescreen=1',
+                '/styles/styles.css',
+                '/scritps.js'
+            ]);
+        })
+    );
+});
+
+
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
